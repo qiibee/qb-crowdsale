2242a2fa93bc2e8b7f77d9b3ea27bd501960a8b0
Aaron Colaço <aaroncolaco.work@gmail.com>
2018-07-06 15:19:35 +0530

    Fix bug where cap values were too low(were being set in wei)

--- migrations/2_crowdsale_migration.js
+++ migrations/2_crowdsale_migration.js
@@ -1,16 +1,16 @@
 const QiibeeCrowdsale = artifacts.require("./QiibeeCrowdsale.sol");
+const Web3 = require('web3');
+const web3 = new Web3();
 
 module.exports = function (deployer) {
     deployer.deploy(QiibeeCrowdsale
-        , 1534508477    // _openingTime - 28 June + 50 days
-        , 1538828508    // _closingTime - 28 June + 100 days
-        , 21
-        , 150000000000
-        , 50000
-        , 800000
-        , 22000000000
-        , '0x9d9e14cc4915520abab7af8bb82226943f26d4fa'  // QiibeeToken address
-        , '0x82f42dc102f0d957053b2c8dbd8516f3d796279f');
+        , 1530868919                                        // _openingTime - 6 July 
+        , 1538828508                                        // _closingTime - 28 June + 100 days
+        , 21                                                // _rate
+        , web3.utils.toWei('100', 'ether')                  // _cap
+        , web3.utils.toWei('0.0000002', 'ether')            // _minContrib
+        , web3.utils.toWei('50', 'ether')                   // _maxCumulativeContrib
+        , web3.utils.toWei('1', 'ether')                    // _maxGasPrice
+        , '0x32905da9a790a641a83bd73f89455f89cbe6f551'      // QiibeeToken address
+        , '0x82f42dc102f0d957053b2c8dbd8516f3d796279f');    // _wallet address
 };
-
-
